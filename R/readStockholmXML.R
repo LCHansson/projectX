3d2aad1726b83961248803b783f9d88e97cad508
Love Hansson <love.hansson@gmail.com>
2014-01-04 16:57:20 +0100

    Code cleanup for build

--- R/readStockholmXML.R
+++ R/readStockholmXML.R
@@ -5,15 +5,11 @@
 #' @param file file path
 #' 
 #' @export
-require(XML)
 readStockholmXML <- function(file){
   doc <- xmlParse(file)
   
   colspec <- unlist(xpathApply(doc, "//odsxml/body/proc/branch/leaf/output/output-object/colspecs/colgroup/colspec/@type"))
   colspec[colspec == "string"] <- "character"
-  
-  
-  
   
   header <- xpathSApply(doc, "//odsxml/body/proc/branch/leaf/output/output-object/output-head/row/header/value/text()", xmlValue)
   
