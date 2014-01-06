94b0f5c1c8611c78531f86539e0ea253a2c6a6fd
Love Hansson <love.hansson@gmail.com>
2014-01-06 13:08:20 +0100

    Added index calculation method

---
+++ R/calculate_index.R
@@ -0,0 +1,35 @@
+#' Calculate BLKI
+#' 
+#' Calculat BoendeLivsKvalitetsIndex (BLKI) for a given address
+#' 
+#' @export
+ 
+blki <- function(street, number) {
+	address <- sthAddr(street, number)
+  
+  # 1. Miljöindex: ENV_IX
+  env_ix <- 50 # We leave this constant for now...
+  
+  # 2. Restidsindex: RT_IX
+  traveltime <- travelTimeFromPos(address$WGS84)
+  
+  rt_ix <- normalizeTravelTime(traveltime)
+  
+  # 3. Fritidsindex: FT_IX
+  leisuredata <- someData
+  
+  ft_ix <- normalizeLeisureData(leisuredata)
+  
+  # 4. Vardagsindex: VD_IX
+  vardagdata <- someData
+  
+  vd_ix <- normalizeVardagData(vardagdata)
+  
+  # Calculate composite index
+  blki_weights <- c(0,0.33,0.33,0.34) # The sum of the weights must add upp to 1.0.
+  blki_partialindexes <- c(env_ix, rt_ix, ft_ix, vd_ix)
+  
+  blki <- sum(blki_weights * blki_partialindexes)
+  
+  return(blki)
+}
