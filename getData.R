4c0f2d0592c16538a79bb7df12f956a33802b7b3
Love Hansson <love.hansson@gmail.com>
2014-01-06 14:06:11 +0100

    Get data and Normalization functions

--- getData.R
+++ getData.R
@@ -5,5 +5,25 @@
 #' @export
 
 getLeisureData <- function(address) {
-  
+  NULL
+}
+
+#' Get Vardag data
+#' 
+#' Get Vardag data
+#' 
+#' @export
+
+getVardagData <- function(address) {
+  NULL
+}
+
+#' Get Environmental data
+#' 
+#' Get Environmental data
+#' 
+#' @export
+
+getEnvironmentalData <- function(address) {
+  NULL
 }
