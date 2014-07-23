1a6473285217abf0772bd155057b6895cf1b2d26
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-07-23 17:11:10 +0200

    Add general design for getData() method

--- R/getData.R
+++ R/getData.R
@@ -1,3 +1,21 @@
+#' Get data from DB
+#' 
+#' This is the main method for getting data. It takes an address argument and some optional arguments as input.
+#' 
+#' @export
+
+getAddress <- function(address) {
+  NULL
+  
+  ## Program flow: ##
+  # lookInDatabaseForAddress()
+  # if (foundAddress)
+  #   verifyAddressIsNotTooOld()
+  # else
+  #   getAddressFromAPI()
+  #   putAddressIntoDatabase()
+}
+
 #' Get Leisure data
 #' 
 #' Get Leisure data
